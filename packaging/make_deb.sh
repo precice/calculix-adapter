eda773ae2e319d022c14ac2c62ff06183975fc0b
Boris Martin <boris.martin.be@gmail.com>
2021-11-21 02:08:30 +0100

    Improved worfklow flexibility, renamed package
    The Debian package is now named "calculix-precice2" and not "calculix-precice". The Github Action workflow was modified to allow more flexible updates.
    There is now a variable which helps reduce the number of things to change to upgrade CalculiX version or package version.

--- packaging/make_deb.sh
+++ packaging/make_deb.sh
@@ -4,23 +4,23 @@ DISTRIBUTION="_$1"
 echo $DISTRIBUTION
 
 # Compress the changelog, strip the binaries
-cp changelog.Debian calculix-precice_2.17-1_amd64/usr/share/doc/calculix-precice/changelog.Debian
+cp changelog.Debian calculix-precice2_2.17-1_amd64/usr/share/doc/calculix-precice2/changelog.Debian
 # Options : --best for best compression, -f for removing file if it was there, -n for no time stamp
-gzip --best -f -n calculix-precice_2.17-1_amd64/usr/share/doc/calculix-precice/changelog.Debian
-strip --strip-unneeded calculix-precice_2.17-1_amd64/usr/bin/ccx_preCICE
+gzip --best -f -n calculix-precice2_2.17-1_amd64/usr/share/doc/calculix-precice2/changelog.Debian
+strip --strip-unneeded calculix-precice2_2.17-1_amd64/usr/bin/ccx_preCICE
 
 #Compile and compress the manual
 
 pandoc manpage.md -s -t man -o ccx_preCICE.1
-mkdir -p calculix-precice_2.17-1_amd64/usr/share/man/man1
+mkdir -p calculix-precice2_2.17-1_amd64/usr/share/man/man1
 gzip -9 -n -f ccx_preCICE.1
 chmod 644 ccx_preCICE.1.gz
-mv ccx_preCICE.1.gz calculix-precice_2.17-1_amd64/usr/share/man/man1
+mv ccx_preCICE.1.gz calculix-precice2_2.17-1_amd64/usr/share/man/man1
 
 # Copy to a folder with appropriate postfix
 
-cp -r calculix-precice_2.17-1_amd64/ "calculix-precice_2.17-1_amd64$DISTRIBUTION"
+cp -r calculix-precice2_2.17-1_amd64/ "calculix-precice2_2.17-1_amd64$DISTRIBUTION"
 
-dpkg-deb --build --root-owner-group "calculix-precice_2.17-1_amd64$DISTRIBUTION"
+dpkg-deb --build --root-owner-group "calculix-precice2_2.17-1_amd64$DISTRIBUTION"
 lintian *.deb
 
