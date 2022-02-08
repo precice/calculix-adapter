41ab20906e2c3034089590cbc323f9dd8df7e48d
Gerasimos Chourdakis <makishourdakis@gmail.com>
2022-02-08 22:10:16 +0100

    Modify script to format all C++ files

--- tools/format-code.sh
+++ tools/format-code.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Format all adapter C and C++ files with clang-format
-find ./adapter/ \( -iname "*.h*" -o -iname "*.c*" \) -exec clang-format -i {} \;
+find ./ \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.c" -o -iname "*.cpp" \) -exec clang-format -i {} \;
