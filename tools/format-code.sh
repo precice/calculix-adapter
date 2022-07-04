37a43df7209946993c4bb3ba51eabca72f090d23
Boris Martin <boris.martin.be@gmail.com>
2022-07-04 22:08:58 +0200

    restored script

--- tools/format-code.sh
+++ tools/format-code.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Format all adapter C and C++ files with clang-format
-find ./ \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.c" -o -iname "*.cpp" \) -exec clang-format-11 -i {} \;
+find ./ \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.c" -o -iname "*.cpp" \) -exec clang-format -i {} \;
