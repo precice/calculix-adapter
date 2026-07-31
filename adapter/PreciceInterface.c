7618350d2ffe44073531decce6b5faf0fb2986d1
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-07-31 11:09:10 +0200

    Adjust comment and formatting

--- adapter/PreciceInterface.c
+++ adapter/PreciceInterface.c
@@ -471,11 +471,11 @@ void PreciceInterface_Create(PreciceInterface *interface, SimulationData *sim, I
     }
   }
 
-  // Initialize counters (see issue #152 on https://github.com/precice/calculix-adapter/issues/152)
-  interface->numNodes              = 0;
-  interface->nodeSetID             = 0;
-  interface->numElements           = 0;
-  interface->faceSetID             = 0;
+  // Initialize counters
+  interface->numNodes    = 0;
+  interface->nodeSetID   = 0;
+  interface->numElements = 0;
+  interface->faceSetID   = 0;
 
   // Initialize pointers as NULL
   interface->elementIDs            = NULL;
