336479ad5789d760d9cc9c765ed5fb698e18479d
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-07-31 11:13:58 +0200

    Fix uninitialized PreciceInterface counters (#165)
    Co-authored-by: Guillaume De Nayer <denayer@hsu-hh.de>

--- adapter/PreciceInterface.c
+++ adapter/PreciceInterface.c
@@ -471,6 +471,12 @@ void PreciceInterface_Create(PreciceInterface *interface, SimulationData *sim, I
     }
   }
 
+  // Initialize counters
+  interface->numNodes    = 0;
+  interface->nodeSetID   = 0;
+  interface->numElements = 0;
+  interface->faceSetID   = 0;
+
   // Initialize pointers as NULL
   interface->elementIDs            = NULL;
   interface->faceIDs               = NULL;
